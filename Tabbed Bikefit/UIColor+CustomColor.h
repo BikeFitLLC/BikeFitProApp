c4c179357911baa980c0a938c04bb3960dfa38a6
Drew <drew@generalui.com>
2016-08-04 12:31:08 -0700

    BIK-5 Add color extension

---
+++ Tabbed Bikefit/UIColor+CustomColor.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+CustomColor.h
+//  bikefit
+//
+//  Created by Developer on 8/4/16.
+//  Copyright © 2016 Alfonso Lopez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIColor (customColor)
+
++ (UIColor *)bikeFitBlue;
+
+@end
---
+++ Tabbed Bikefit/UIColor+CustomColor.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+CustomColor.m
+//  bikefit
+//
+//  Created by Developer on 8/4/16.
+//  Copyright © 2016 Alfonso Lopez. All rights reserved.
+//
+
+#import "UIColor+CustomColor.h"
+
+@implementation UIColor (customColor)
+
++ (UIColor *)bikeFitBlue {
+    return [UIColor colorWithRed:0x7/255.0 green:0x31/255.0 blue:0x54/255.0 alpha:1.0];
+}
+
+@end

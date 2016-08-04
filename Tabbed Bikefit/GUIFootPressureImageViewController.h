0c941acea658388c32c29bf4ecbee51fd5f6301b
Drew <drew@generalui.com>
2016-08-04 10:58:02 -0700

    BIK-5/Add_foot_pressure_image_controller

---
+++ Tabbed Bikefit/GUIFootPressureImageViewController.h
@@ -0,0 +1,16 @@
+//
+//  GUIFootPressureImageViewController.h
+//  bikefit
+//
+//  Created by Developer on 8/4/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GUIFootPressureImageViewController : UIViewController
+
+@property (nonatomic, assign) NSUInteger index;
+
+- (void)setImage:(UIImage *)image;
+
+@end
